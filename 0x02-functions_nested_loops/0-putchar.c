825b993de807b78e147161015b3b1410bd76eb11
Osegbo Kennedy <kizzy726@gmail.com>
2022-09-14 01:28:15 +0300

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- *  * main - Check description
- *   * Description: It prints the word _putchar, followed by a new line.
- *    * Return: 0.
- *     */
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
 int main(void)
 {
-	char word[8] = "-putchar";
+	char word[8] = "_putchar";
 	int i;
 	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
